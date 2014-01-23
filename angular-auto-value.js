058d1369a0c02dc826a4c30c7328e2ccb062fbe0
John Wright <johngeorge.wright@gmail.com>
2014-01-23 10:06:49 +0000

    Removing the date formatting
    As far as I'm concerned the value would have to be added correctly for
    the input to work properly without javascript... so why rely on
    javascript to get it working.

--- angular-auto-value.js
+++ angular-auto-value.js
@@ -22,15 +22,6 @@
       case "checkbox":
         this.updateCheckbox();
         break;
-      case "date":
-        this.updateDate();
-        break;
-      case "datetime-local":
-        this.updateDatetimeLocal();
-        break;
-      case "month":
-        this.updateMonth();
-        break;
       case "number":
       case "range":
         this.updateNumber();
@@ -59,62 +50,8 @@
     }
   };
   
-  AutoInputValueCtrl.prototype.updateDate = function () {
-    var val = new Date(this.val),
-        year, month, date, str;
-    if (this.isValidDateInstance(val)) {
-      year = val.getFullYear();
-      month = this.padLeft(val.getMonth() + 1);
-      date = this.padLeft(val.getDate());
-      str = year + "-" + month + "-" + date;
-      this.setter(this.$scope, str);
-    }
-  };
-  
-  AutoInputValueCtrl.prototype.updateDatetimeLocal = function () {
-    var val = new Date(this.val),
-        isoString;
-    if (this.isValidDateInstance(val)) {
-      isoString = val.toISOString().replace(/Z.*/, "");
-      this.setter(this.$scope, isoString);
-    }
-  };
-  
-  AutoInputValueCtrl.prototype.updateMonth = function () {
-    var val = new Date(this.val),
-        year, month, str;
-    if (this.isValidDateInstance(val)) {
-      year = val.getFullYear();
-      month = this.padLeft(val.getMonth() + 1);
-      str = year + "-" + month;
-      this.setter(this.$scope, str);
-    }
-  };
-  
   AutoInputValueCtrl.prototype.updateNumber = function () {
     this.setter(this.$scope, parseInt(this.val));
-  };
-  
-  AutoInputValueCtrl.prototype.padLeft = function (str, min, char) {
-    if (!min) {
-      min = 2;
-    }
-    if (!char) {
-      char = 0;
-    }
-    str = str.toString();
-    while (str.length < min) {
-      str = char + str;
-    }
-    return str;
-  };
-  
-  AutoInputValueCtrl.prototype.isValidDateInstance = function (inst) {
-    var valid = false;
-    if (Object.prototype.toString.call(inst) === "[object Date]" && !isNaN(inst.getTime())) {
-      valid = true;
-    }
-    return valid;
   };
   
   function autoInputValueDirective() {
