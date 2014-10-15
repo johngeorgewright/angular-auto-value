49260b8c3f866c9811e24b5091f4c1303e80185b
John Wright <johngeorge.wright@gmail.com>
2014-10-15 12:15:26 +0100

    Update pre-commit to only run tests when needed to.

--- git-hooks/pre-commit.sh
+++ git-hooks/pre-commit.sh
@@ -13,7 +13,7 @@ command()
 changed_testable_files=`git diff --cached --name-only | grep test`
 changed_minifiable_file=`git diff --cached --name-only | grep angular-auto-value.js`
 
-if [$changed_testable_files || $changed_minifiable_file]
+if [$changed_testable_files] || [$changed_minifiable_file]
 then
   command npm test
 fi
